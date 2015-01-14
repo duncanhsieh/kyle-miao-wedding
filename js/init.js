698e8d9cc51b749963f2e95736072d3e8acf5cff
Duncan Hsieh <duncan.hsieh@fanzytv.com>
2015-01-14 14:14:16 +0800

    no message

--- js/init.js
+++ js/init.js
@@ -15,7 +15,7 @@ $(function(){
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) {return;}
     js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/zh_TW/sdk.js";
+    js.src = "//connect.facebook.net/zh_TW/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 
